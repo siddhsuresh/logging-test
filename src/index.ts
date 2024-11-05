476f253ac2fc7f9bde03299af624bf13701e5bf7
Siddharth Suresh <siddh.suresh@gmail.com>
2024-11-05 18:01:03 +0530

    add dockerfile

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Elysia } from "elysia";
 const app = new Elysia().get("/", () => {
   console.log("Test log");
   return "Hello, Elysia!";
-}).listen(3000);
+}).listen(process.env.PORT ?? 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
